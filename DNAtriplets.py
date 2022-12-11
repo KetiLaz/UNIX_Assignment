4a1dd883d33376b0dc65035338d2b905b4af7e88
Keti Lazarashvili <evdoksiaketevan@gmail.com>
2022-12-11 18:08:00 +0200

    Some changes to the logic

--- DNAtriplets.py
+++ DNAtriplets.py
@@ -6,8 +6,6 @@ Created on Mon Dec  5 12:53:49 2022
 @author: EvdoksiaKetevan
 """
 
-finish_codons = ['TAA', 'TAG', 'TGA']
-
 DNAseq = input("Give me your DNA sequence:\n").upper()
 
 orf = []
@@ -24,30 +22,46 @@ def find_start_codon():
     if check_sequence() == True:
         for i in range(len(DNAseq)):
             if DNAseq[i] == 'A' and DNAseq[i+1] == 'T' and DNAseq[i+2] == 'G':
-                print("We have the starting codon ATG starting in {} and ending in {}!".format(i,i+2))
                 return i+2
                 return True
+                print("There is a start codon")
     return False
-
-def find_finish_codon():
+    
+def add_codons():
     if check_sequence() == True and find_start_codon() != False:
         start_codon_ending = find_start_codon() + 1 
         for j in range(start_codon_ending, len(DNAseq), 3):
             if len(DNAseq[j:j+3]) % 3 == 0:
                 codon = DNAseq[j:j+3]
-                if codon not in finish_codons:
-                    orf.append(codon)
-                else: 
-                    break
+                orf.append(codon)
     print(orf)
                     
     
 
-                    
-                
-        
-find_finish_codon()
+def find_finish_codon():
+    for i in range(len(orf)):
+        if 'TAA' in orf:
+            taa = orf.index('TAA')
+            print(orf[:taa])
+            break
+        elif 'TGA' in orf:
+            tga = orf.index('TGA')
+            print(orf[:tga])
+            break
+        elif 'TAG' in orf:
+            tag = orf.index('TAG')
+            print(orf[:tag])
+            break
+        else:
+            print("We have no ORF!")
+            break
+            
+    
+    
+    
 
+add_codons()
+find_finish_codon()
         
 
 
