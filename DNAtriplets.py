8bcd45ee247962cea0dbee74e0d3b22a7825ebe8
Keti Lazarashvili <evdoksiaketevan@gmail.com>
2023-01-04 19:23:36 +0200

    Fixed the IndexError

--- DNAtriplets.py
+++ DNAtriplets.py
@@ -10,6 +10,7 @@ DNAseq = input("Give me your DNA sequence:\n").upper()
 
 orf = []
 
+
 def check_sequence():
     for char in DNAseq:
         if char not in 'ATGC':
@@ -20,47 +21,44 @@ def check_sequence():
 
 def find_start_codon():
     if check_sequence() == True:
-        for i in range(len(DNAseq)):
-            if DNAseq[i] == 'A' and DNAseq[i+1] == 'T' and DNAseq[i+2] == 'G':
-                return i+2
-                return True
-                print("There is a start codon")
-    return False
+        for i in range(0, len(DNAseq)):
+            if(DNAseq[i : i+3] == 'ATG'):
+                return i+3
+                break
     
 def add_codons():
-    if check_sequence() == True and find_start_codon() != False:
-        start_codon_ending = find_start_codon() + 1 
-        for j in range(start_codon_ending, len(DNAseq), 3):
-            if len(DNAseq[j:j+3]) % 3 == 0:
-                codon = DNAseq[j:j+3]
-                orf.append(codon)
-    print(orf)
+    if check_sequence() == True:
+        start_codon_ending = find_start_codon() 
+        if start_codon_ending is not None:
+            for j in range(start_codon_ending, len(DNAseq), 3):
+                if len(DNAseq[j:j+3]) % 3 == 0:
+                    codon = DNAseq[j:j+3]
+                    orf.append(codon)
+        else:
+            print("There is no ORF")
+    return orf
                     
     
-
 def find_finish_codon():
+    add_codons()
     for i in range(len(orf)):
         if 'TAA' in orf:
             taa = orf.index('TAA')
-            print(orf[:taa])
+            print("The ORf is: {}".format(orf[:taa]))
             break
         elif 'TGA' in orf:
             tga = orf.index('TGA')
-            print(orf[:tga])
+            print("The ORf is: {}".format(orf[:tga]))
             break
         elif 'TAG' in orf:
             tag = orf.index('TAG')
-            print(orf[:tag])
+            print("The ORf is: {}".format(orf[:tag]))
             break
         else:
-            print("We have no ORF!")
+            print("We don't have an ORF")
             break
             
-    
-    
-    
 
-add_codons()
 find_finish_codon()
         
 
